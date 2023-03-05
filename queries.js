10f6344d23f77f7a908dc9bdbef288b64a31f547
nikhil5022 <gsn.agc5022@gmail.com>
2023-03-05 16:42:00 +0530

    get movies by id added

--- queries.js
+++ queries.js
@@ -120,3 +120,4 @@ module.exports.deleteUser=deleteUser
 module.exports.addUser = addUser
 module.exports.addMovie = addMovie
 module.exports.getMovieByFullName = getMovieByFullName
+module.exports.getMoviebyId=getMoviebyId
