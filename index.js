cc1cc82ba54df09dc632c760c7ae12c0330cca18
nikhil5022 <gsn.agc5022@gmail.com>
2023-03-05 22:25:19 +0530

    unfollow added

--- index.js
+++ index.js
@@ -115,6 +115,18 @@ app.post("/watchlist/:movieid/:username",(req,res)=>{
         res.send(response)
     })
 })
+
+
+app.put("/unFollowUser/:username1/:username2",(req,res)=>{
+    user1=req.params.username1
+    user2=req.params.username2
+    queries.unfollowuser(user1,user2).then(response=>{
+        if(response=="user not found"||response=="user does not exist in followers list"){
+            res.send({"message":response})
+        }
+        res.send(response)
+    })
+})
 app.listen(port,()=>{
     console.log('listening at port '+port)
 })
